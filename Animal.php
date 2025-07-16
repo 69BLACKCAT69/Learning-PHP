d95996f2e30eecee6f33c5fbaf9f4a9bf269a831
Santiago Sosa Trejo <129685011+69BLACKCAT69@users.noreply.github.com>
2025-07-16 12:13:10 -0600

    Update Animal.php

--- Animal.php
+++ Animal.php
@@ -1,1 +1,19 @@
+<?php
+class Animal {
+    protected string $nombre;
+    protected int $edad;
 
+    public function __construct(string $nombre, int $edad) {
+        $this->nombre = $nombre;
+        $this->edad = $edad;
+    }
+
+    public function describir(): string {
+        return "Soy {$this->nombre} y tengo {$this->edad} años.";
+    }
+
+    public function hacerSonido(): string {
+        return "Hago un sonido genérico.";
+    }
+}
+?>
