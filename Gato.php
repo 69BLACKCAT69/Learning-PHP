9e2f4cf63e22feb7582f5f754bee607817e332af
Santiago Sosa Trejo <129685011+69BLACKCAT69@users.noreply.github.com>
2025-07-16 12:22:50 -0600

    Update Gato.php

--- Gato.php
+++ Gato.php
@@ -1,6 +1,11 @@
 <?php
+require_once 'Animal.php';
+require_once 'traits/Come.php';
+
 class Gato extends Animal {
-    public function hacerSonido() {
+    use Come;
+
+    public function hacerSonido(): string {
         return "{$this->nombre} dice: ¡Miau!";
     }
 }
