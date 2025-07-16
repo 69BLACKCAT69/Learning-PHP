3cec0aa8c179da32bdfd89fd2cfc149fe3780c15
Santiago Sosa Trejo <129685011+69BLACKCAT69@users.noreply.github.com>
2025-07-16 12:17:42 -0600

    Update Perro.php

--- Perro.php
+++ Perro.php
@@ -1,7 +1,17 @@
 <?php
-class Perro extends Animal {
-    public function hacerSonido() {
+require_once 'Animal.php';
+require_once 'interfaces/Corredor.php';
+require_once 'traits/Come.php';
+
+class Perro extends Animal implements Corredor {
+    use Come;
+
+    public function hacerSonido(): string {
         return "{$this->nombre} dice: ¡Guau!";
+    }
+
+    public function correr(): string {
+        return "{$this->nombre} está corriendo feliz 🐕";
     }
 }
 ?>
